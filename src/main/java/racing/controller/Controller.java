7877f6cda398a3aee81f22a22358be4e51993633
hapdaypy <mgim9316@gmail.com>
2026-03-15 17:54:39 +0900

    fix: application 사용에서 psvm으 변경

--- src/main/java/racing/controller/Controller.java
+++ src/main/java/racing/controller/Controller.java
@@ -7,19 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    public void run(){
-        //[1] 자동차, 경기진행 횟수 입력
+    public static void main(String[] args) {
+
+        // [1] 자동차 이름 및 시도 횟수 입력 받기
         String carNameInput = InputView.inputCarName();
         int trialNumber = InputView.inputTrialNumberCount();
-
-        //[2] 데이터 변환 : 문자열 -> 자동차 객체 배열
+        System.out.println("입력된 이름: " + carNameInput); //디버깅용
+        // [2] 데이터 변환: 문자열 -> 자동차 객체 리스트
         List<Car> cars = createCars(carNameInput);
     }
-    private List<Car> createCars(String input){
+
+    private static List<Car> createCars(String input) {
         String[] names = input.split(",");
         List<Car> cars = new ArrayList<>();
-
-        for(String name : names) {
+        for (String name : names) {
             cars.add(new Car(name.trim()));
         }
         return cars;
