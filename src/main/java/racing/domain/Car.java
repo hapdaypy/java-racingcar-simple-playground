1c4dfe54fd6097acc19e8c1167c5485d1820cf0f
hapdaypy <mgim9316@gmail.com>
2026-03-15 15:24:41 +0900

    feat: car 객체 이름 추가 기능 생성

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,7 +1,11 @@
-package domain;
+package racing.domain;
 
 public class Car {
     private final String name;
     private int position  = 0;
     private boolean isWinner = false;
+
+    public Car(String name){
+        this.name = name;
+    }
 }
