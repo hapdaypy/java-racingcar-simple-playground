11a0d073b7d8f6607ef94f150dad0e97e1c2d922
hapdaypy <mgim9316@gmail.com>
2026-03-15 18:25:02 +0900

    feat: 자동차 객체 기능 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -8,4 +8,38 @@ public class Car {
     public Car(String name){
         this.name = name;
     }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.");
+        }
+    }
+
+    public void move() {
+        this.position++;
+    }
+
+    public String getPositionVisualized() {
+        StringBuilder visualPath = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            visualPath.append("-");
+        }
+        return visualPath.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public boolean isWinner() {
+        return isWinner;
+    }
+
+    public void setWinner(boolean winner) {
+        isWinner = winner;
+    }
 }
