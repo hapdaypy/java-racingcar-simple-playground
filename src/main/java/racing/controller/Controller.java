23abbbe5016497a59cb131711e0af18807f0b3a5
hapdaypy <mgim9316@gmail.com>
2026-03-15 21:34:54 +0900

    feat: 경기를 진행시키고 우승자를 출력하는 기능 추가

--- src/main/java/racing/controller/Controller.java
+++ src/main/java/racing/controller/Controller.java
@@ -1,27 +1,33 @@
 package racing.controller;
 
 import racing.view.InputView;
+import racing.view.OutputView;
+
 import racing.domain.Car;
+import racing.domain.Race;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
     public static void main(String[] args) {
-
         // [1] 자동차 이름 및 시도 횟수 입력 받기
         String carNameInput = InputView.inputCarName();
         int trialNumber = InputView.inputTrialNumberCount();
-        System.out.println("입력된 이름: " + carNameInput); //디버깅용
         // [2] 데이터 변환: 문자열 -> 자동차 객체 리스트
-        List<Car> cars = createCars(carNameInput);
+        var cars = createCars(carNameInput); //cars 라는 자동차 객체 배열 생성
+        //[3] 레이씽 경기 시작
+        Race.start(cars,trialNumber);
+        //[4] 결과 출력
+        List<String> winners = Race.getWinners(cars);
+        OutputView.printWinners(winners);
     }
-
     private static List<Car> createCars(String input) {
         String[] names = input.split(",");
-        List<Car> cars = new ArrayList<>();
+        var cars = new ArrayList<Car>();
         for (String name : names) {
-            cars.add(new Car(name.trim()));
+            cars.add(new Car(name));
         }
         return cars;
     }
