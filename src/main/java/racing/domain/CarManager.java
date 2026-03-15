2fa8c6465156bce60addcea159a798be8671daf4
hapdaypy <mgim9316@gmail.com>
2026-03-15 22:33:20 +0900

    feat: CarManager를 통하여 자동차 객체 관리

---
+++ src/main/java/racing/domain/CarManager.java
@@ -0,0 +1,24 @@
+package racing.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarManager {
+    public static List<Car> createCars(String input) {
+        String[] names = input.split(",");
+        var cars = new ArrayList<Car>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+    public static void Duplicate(List<Car> cars){
+        long distinctCount = cars.stream()
+                .map(Car::getName) // Car 객체에서 이름을 추출한다고 가정
+                .distinct()
+                .count();
+        if (distinctCount != cars.size()) {
+            throw new IllegalArgumentException("[ERROR]중복된 자동차 이름이 존재합니다.");
+        }
+    }
+}
