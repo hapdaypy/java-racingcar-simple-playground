82e46fbb50f471b0e94646cccd7b8b8171499f34
hapdaypy <mgim9316@gmail.com>
2026-03-15 20:56:01 +0900

    refactor: 잘못된 호출 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputCarName() {
-        System.out.println(INPUT_CAR_COUNT_MASSAGE);
+        System.out.println(INPUT_CAR_NAME_MASSAGE);
         return scanner.nextLine();
     }
 
