75fbb12e3408711cd2b77b27c629a64906138a6d
hapdaypy <mgim9316@gmail.com>
2026-03-23 11:49:39 +0900

    refactor: scanner를 메서드 내부에서 호출, 지역 변수화

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -5,9 +5,10 @@ public class InputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분.)";
     private static final String INPUT_CAR_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final Scanner scanner = new Scanner(System.in);
+
 
     public static String inputCarName() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         String carName = scanner.nextLine();
         validateCarNameFormat(carName);
@@ -16,6 +17,7 @@ public class InputView {
     }
 
     public static int inputTrialNumberCount() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(INPUT_CAR_COUNT_MESSAGE);
         String input = scanner.nextLine();
         return parseTrialCount(input);
