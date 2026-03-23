2e6d2ccf1d6e8ff50048a7b8d3d5eab07c75f9db
hapdaypy <mgim9316@gmail.com>
2026-03-23 11:07:02 +0900

    feat: random에 따라 움직이는 코드를 임의의 값을 주입하여 참/거짓을 올바르게 반환하는지 검토하는 테스트 코드 작성

---
+++ src/test/java/racing/domain/RandomMoveStrategyTest.java
@@ -0,0 +1,41 @@
+package racing.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomMoveStrategyTest {
+
+    @Test
+    @DisplayName("생성된 난수가 4 이상이면 참을 반환한다.")
+    void isMovable_True_WhenRandomIs4OrMore() {
+        // 통제된 난수 4를 반환하는 익명 객체 주입
+        Random controlledRandom = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 4;
+            }
+        };
+        RandomMoveStrategy moveStrategy = new RandomMoveStrategy(controlledRandom);
+
+        assertThat(moveStrategy.isMovable()).isTrue();
+    }
+
+    @Test
+    @DisplayName("생성된 난수가 3 이하면 거짓을 반환한다.")
+    void isMovable_False_WhenRandomIs3OrLess() {
+        // 통제된 난수 3을 반환하는 익명 객체 주입
+        Random controlledRandom = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 3;
+            }
+        };
+        RandomMoveStrategy moveStrategy = new RandomMoveStrategy(controlledRandom);
+
+        assertThat(moveStrategy.isMovable()).isFalse();
+    }
+}
