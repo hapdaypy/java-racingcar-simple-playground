ebf44d7ff2a7ef3bac3e2bde0d23d8d6e4516312
hapdaypy <mgim9316@gmail.com>
2026-03-28 20:35:11 +0900

    fix: 바뀐 계층 수정

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -2,6 +2,7 @@ package racing.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racing.view.InputView;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ class CarsTest {
     @Test
     @DisplayName("중복된 이름의 자동차가 포함되어 있으면 예외가 발생한다.")
     void validateDuplicate_ThrowsException() {
-        List<Car> carList = CarParser.parse("pobi,pobi,woni");
+        List<Car> carList = InputView.parse("pobi,pobi,woni");
 
         assertThatThrownBy(() -> new Cars(carList))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -23,7 +24,7 @@ class CarsTest {
     @Test
     @DisplayName("정상적인 자동차 리스트가 주어지면 Cars 객체가 생성된다.")
     void createCars_Success() {
-        List<Car> carList = CarParser.parse("pobi,woni,jun");
+        List<Car> carList = InputView.parse("pobi,woni,jun");
 
         Cars cars = new Cars(carList);
 
@@ -33,7 +34,7 @@ class CarsTest {
     @Test
     @DisplayName("getCarList로 반환된 리스트는 불변(Immutable)이어야 한다.")
     void getCarList_IsImmutable() {
-        List<Car> carList = CarParser.parse("pobi,woni");
+        List<Car> carList = InputView.parse("pobi,woni");
         Cars cars = new Cars(carList);
 
         List<Car> returnedList = cars.getCarList();
